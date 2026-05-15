fe8f709083153d1512fb8d6dda8de3ebb76ef97e
Acty77 <yo.naranjo@duocuc.cl>
2026-05-14 21:16:24 -0400

    Update JwtUtil.java

--- src/main/java/cl/duoc/ms_eventos/security/JwtUtil.java
+++ src/main/java/cl/duoc/ms_eventos/security/JwtUtil.java
@@ -5,6 +5,9 @@ import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 /*
  * Clase utilitaria para leer y validar tokens JWT.

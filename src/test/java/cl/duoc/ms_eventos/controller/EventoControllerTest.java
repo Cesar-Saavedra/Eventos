a33f28b619750b7bf864457707a4499c093e5fc7
TomasB <to.blanchard@duocuc.cl>
2026-06-30 15:44:13 -0400

    Arreglo de Errores
    no compilaba proyecto por falta de compatibilidad

--- src/test/java/cl/duoc/ms_eventos/controller/EventoControllerTest.java
+++ src/test/java/cl/duoc/ms_eventos/controller/EventoControllerTest.java
@@ -10,7 +10,8 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -32,6 +33,7 @@ import cl.duoc.ms_eventos.security.JwtUtil;
 import cl.duoc.ms_eventos.service.EventoService;
 
 @WebMvcTest(EventoController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class EventoControllerTest {
 
     @Autowired
